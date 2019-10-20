f89810ecf30a2b150cbdc142cbd911f09c4d780f
Jatinokov <Enkhtaivan1206@gmail.com>
2019-10-19 23:46:36 -0700

    setInterval time , once reach '0' it is estimates correct & incorrect value of each ratio, in html- set family-font

--- js/app.js
+++ js/app.js
@@ -1,55 +1,125 @@
-
-var number = 5;
-
+// start counting down 
+var number = 30;
 var intervalId;
-
+// start button for start count down seconds 
 $('#btn-Start').on('click', run);
+// $('#btn-Done').on('click', stop);
 
-$('#btn-Done').on('click', result)
-
-
+// main function for decreasing number
 function run(){
     clearInterval(intervalId);
     intervalId = setInterval(decrement, 1000);
 }
+// once seconds reach to 0 it will stop
+function stop(){
+    clearInterval(intervalId);
 
+}
+
+// giving correct & incorrect answer value it starts 0
+var Correct = 0;
+var Incorrect = 0;
 
 function decrement(){
  
    number--;
-
+// desplaying seconds
     $("#time-left").html(number);
     // console.log(number)
+// conditional statement for each questions for cheching correct and incorrect answer
     if(number === 0){
-        stop();
-        
-        if('#5'=== 5){
-            correct++;
-        }else{
-            incorrect++
+         stop();
+         
+// 1дахь асуулт
+        if($('#5').is(':checked')){
+            // alert("it's checked");
+            Correct++;
+            $('#correct-answer').html(Correct);
         }
+        else{
+            Incorrect++;
+            $('#incorrect-answer').html(Incorrect);
+               
+        }
+    
+// 2дахь асуулт 
+        if($('#pacific').is(':checked')){
+            // alert("it's checked");
+            Correct++;
+            $('#correct-answer').html(Correct);
+        }
+        else{
+            Incorrect++;
+            $('#incorrect-answer').html(Incorrect);
+            //    alert("wrong");
+        }
+        
+// 3дахь асуулт
+        if($('#astralia').is(':checked')){
+            // alert("it's checked");
+            Correct++;
+            $('#correct-answer').html(Correct);
+        }
+        else{
+            Incorrect++;
+            $('#incorrect-answer').html(Incorrect);
+            //    alert("wrong");
+        }
+// 4дахь асуулт
+        if($('#23').is(':checked')){
+            // alert("it's checked");
+            Correct++;
+            $('#correct-answer').html(Correct);
+        }
+        else{
+            Incorrect++;
+            $('#incorrect-answer').html(Incorrect);
+            //    alert("wrong");
+        }
+
+// 5дахь асуулт
+        if($('#nile').is(':checked')){
+            // alert("it's checked");
+            Correct++;
+            $('#correct-answer').html(Correct);
+        }
+        else{
+            Incorrect++;
+            $('#incorrect-answer').html(Incorrect);
+            //    alert("wrong");
+        }
+
+// 6дахь асуулт
+        if($('#365').is(':checked')){
+            // alert("it's checked");
+            Correct++;
+            $('#correct-answer').html(Correct);
+        }
+        else{
+            Incorrect++;
+            $('#incorrect-answer').html(Incorrect);
+            //    alert("wrong");
+        }
+// 
+// Done button!
+//  function done(){
+//     clearInterval(intervalId);
+     
+// }
+
+
+// if($('#btn-Done').on('click', done)){
+//     Correct++;
+// }
+// else{
+//     Incorrect++;
+  
+  
+//  }
+
+
+
+
     }
 }
-
-function stop(){
-    clearInterval(intervalId);
-}
-
-function result(){
-
-}
-
-// var thirtysec = setTimeout(thirtySeconds, 1000*3);
-
-// function thirtySeconds(){
-//     $("#time-left").html(number);
-
-//     // alert('hi');
-//     console.log('hello ');
-
-// var count = thirtysec, 
-
-// var thirtysec = Math.floor((count % (1000 * 60)) / 1000);
-
-
 
